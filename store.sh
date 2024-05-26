b7c4a4fce056676576a53bf89a011fccac2ca61c
lumR19 <120276705+lumR19@users.noreply.github.com>
2024-05-26 04:42:48 +0300

    final update --> lamar

--- store.sh
+++ store.sh
@@ -222,8 +222,9 @@ done
                 echo "Card information saved."
             else
                 echo "Card information not saved."
+                break
             fi
-            break
+            
         else
             echo "Invalid card number. Please try again."
             continue
@@ -261,55 +262,68 @@ calculate_total_price1() {
 }
 
 # Display order summary
-echo "_____________________________________"
-echo "Order Summary:"
-echo "Order Number: $order_number"
-echo "Name: $name"
-echo "Phone: $phone_number"
-echo "Location: $location"
+while true; do
+if [ "$pay_choice" == "y" ]; then
+     echo "_____________________________________"
+     echo "Order Summary:"
+     echo "Order Number: $order_number"
+     echo "Name: $name"
+     echo "Phone: $phone_number"
+     echo "Location: $location"
 
-if [ "$delivery_or_pickup_choice" == "1" ]; then
-    echo "Delivery/Pickup: Delivery"
-else
-    echo "Delivery/Pickup: Pickup"
-fi
-
-if [ "$delivery_or_pickup_choice" = "1" ]; then
-    echo "Delivery Date: $delivery_date"
-else
-    echo "Pickup Date: $pickup_date"
-fi
-
-for ((i=0; i<${#flower_types[@]}; i++)); do
-    echo "Flower: ${flower_quantities[$i]} x ${flower_types[$i]} (${flower_prices[${flower_types[$i]}]} SAR each)"
-done
-echo "Wrapping: $wrap_color with $ribbon_color ribbon (25 SAR)"
-
-# Function to calculate total price with wrapping
-calculate_total_price1() {
-    local flower_total=0
-    for i in "${!flower_types[@]}"; do
-        price_per_unit=${flower_prices[${flower_types[$i]}]}
-        quantity="${flower_quantities[$i]}"
-        flower_total=$(echo "scale=2; $flower_total + $price_per_unit * $quantity" | bc)
-    done
-    
-    local wrapping_price=25
-    local final_price=$(echo "scale=2; $flower_total + $wrapping_price" | bc)
-    echo "$final_price"
-}
-# Calculate total price
-final_price=$(calculate_total_price1)
-echo "Total Price: $final_price SAR"
-# Check if delivery was chosen
-if [ "$delivery_or_pickup_choice" = "1" ]; then
-    # Display success message if payment was successful
-    if [ "$payment_method" = "1" ] || [ "$payment_method" = "3" ]; then
-        echo "Thank you, the delivery representative will contact you to deliver it to your door."
+    if [ "$delivery_or_pickup_choice" == "1" ]; then
+        echo "Delivery/Pickup: Delivery"
     else
-        echo "Your order will be ready for pickup on $delivery_date $pickup_date."
-       
+        echo "Delivery/Pickup: Pickup"
     fi
-else
-    echo "Your order will be ready for pickup on $delivery_date $pickup_date."
-fi
+
+    if [ "$delivery_or_pickup_choice" = "1" ]; then
+        echo "Delivery Date: $delivery_date"
+    else
+        echo "Pickup Date: $pickup_date"
+    fi
+
+    for ((i=0; i<${#flower_types[@]}; i++)); do
+        echo "Flower: ${flower_quantities[$i]} x ${flower_types[$i]} (${flower_prices[$   {flower_types[$i]}]} SAR each)"
+    done
+    echo "Wrapping: $wrap_color with $ribbon_color ribbon (25 SAR)"
+
+    # Function to calculate total price with wrapping
+    calculate_total_price1() {
+        local flower_total=0
+        for i in "${!flower_types[@]}"; do
+            price_per_unit=${flower_prices[${flower_types[$i]}]}
+            quantity="${flower_quantities[$i]}"
+            flower_total=$(echo "scale=2; $flower_total + $price_per_unit * $quantity" | bc)
+        done
+    
+     local wrapping_price=25
+     local final_price=$(echo "scale=2; $flower_total + $wrapping_price" | bc)
+     echo "$final_price"
+     }
+    # Calculate total price
+    final_price=$(calculate_total_price1)
+    echo "Total Price: $final_price SAR"
+    # Check if delivery was chosen
+    if [ "$delivery_or_pickup_choice" = "1" ]; then
+    # Display success message if payment was successful
+        if [ "$payment_method" = "1" ] || [ "$payment_method" = "3" ]; then
+             echo "Thank you, the delivery representative will contact you to deliver it to your door."
+        else
+    
+             echo "Your order will be ready for pickup on $pickup_date $delivery_date ."
+        
+        fi
+    
+    else
+
+        echo "Your order will be ready for pickup on $pickup_date $delivery_date ."
+    
+    fi
+    break
+ else
+
+   break
+   
+  fi
+ done
